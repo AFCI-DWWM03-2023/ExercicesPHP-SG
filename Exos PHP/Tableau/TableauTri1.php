dd64584495456f4779da0d9ded0415a6de439e94
Steven-Grecescu <steven.grecescu@gmail.com>
2023-10-27 11:53:23 +0200

    Ajout exo tri tableau

---
+++ Exos PHP/Tableau/TableauTri1.php
@@ -0,0 +1,25 @@
+<?php
+    echo("------Exo 1------" . "\n");
+    $estVrai = true;
+    $nombre=[];
+    $taille = readline("Taille : ". "\n");
+    for($i=0;$i<$taille;$i++){
+        $saisie = readline("nombre : ". "\n");
+        $nombre[$i] = $saisie;
+    }
+    for($i=0;$i<=count($nombre)-2;$i++){
+        if($nombre[$i+1] == $nombre[$i+1]){
+            $estVrai = true;
+
+        }else{
+            $estVrai = false;
+            break;
+        }
+
+
+    }
+    if($estVrai == false){
+        echo("Les nombrs ne se suivent pas !!");
+    }else{
+        echo("Les nombres se suivent !!");
+    }
---
+++ Exos PHP/Tableau/TableauTri2.php
@@ -0,0 +1,57 @@
+<?php
+
+echo("------Exo 2------" . "\n");
+
+// -----------TRI BULLE--------------
+
+$estVrai = true;
+$nombre=[];
+$taille = readline("Taille : ". "\n");
+for($i=0;$i<$taille;$i++){
+    $saisie = readline("nombre : ". "\n");
+    $nombre[$i] = $saisie;
+}
+while($estVrai){
+$estVrai = false;
+for($i=0;$i<=count($nombre)-2;$i++){
+    if($nombre[$i] < $nombre[$i+1]){
+        $temp = $nombre[$i];
+        $nombre[$i] = $nombre[$i+1];
+        $nombre[$i+1] = $temp;
+        $estVrai = true;
+
+        }
+    }
+}
+foreach($nombre as $valeur){
+    echo ($valeur . "\n");
+}
+
+
+
+
+
+// -----------TRI PAR SELECTION--------------
+
+$min = 0;
+$nombre2=[];
+$taille = readline("Taille : ". "\n");
+for($i=0;$i<$taille;$i++){
+    $saisie = readline("nombre : ". "\n");
+    $nombre2[$i] = $saisie;
+}
+
+    for($i=0;$i<=count($nombre2)-2;$i++){
+        $min = $i;
+        for($j=0;$i+1 < $taille-1;$j++){
+            if($nombre2[$j] < $nombre2[$min]){
+                $min = $j;
+            }
+
+        }
+        if ($min =! $i){
+            $nombre2[$i] = $nombre2[$min];
+        }
+
+
+    }
